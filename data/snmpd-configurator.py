e3a3b4ca39d76448ed6952424749bd97855f63db
Giuliano Peccetto <giuliano.peccetto@intecnus.org.ar>
2022-03-11 16:20:07 -0300

    + Generate MiB extended object HA id based

--- data/snmpd-configurator.py
+++ data/snmpd-configurator.py
@@ -5,7 +5,6 @@ import json
 import subprocess
 from requests import get
 
-
 configFile = sys.argv[1]
 OIDPrefix = sys.argv[2]
 SupervisorToken = os.environ["SUPERVISOR_TOKEN"]
@@ -24,17 +23,11 @@ ha_sensors = json.loads(ha_sensors_request.text)
 for sensor in ha_sensors:
     sensorID = sensor["entity_id"]
 
-    if sensorID == "sun.sun":
-        sensorOID = "1.1.1.1.1.1.1.1.1.1"
-    else:
-        sensorOID = OIDPrefix+sensorID
     # Generate OID
-    #extOID = subprocess.Popen(["snmpget", "", "-arg2"])
-
-
+    sensorOID = subprocess.check_output('snmptranslate -On NET-SNMP-EXTEND-MIB::nsExtendOutput1Line.\"' + sensorID + '\"', shell=True)
 
     # Save
-    configFileObject.write("extend " + sensorOID + " " + sensorID + " python3 get-sensor-data.py")
+    configFileObject.write("extend " + sensorID + " /bin/echo hello"+ '\n')
     print("Added SNMP sensor "+sensorID+" with OID: "+sensorOID)
 
 
